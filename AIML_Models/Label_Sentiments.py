bef13ebc22bf13adfee0d809bc8f67593b43b678
chongsun2002 <119829137+chongsun2002@users.noreply.github.com>
2024-01-11 02:35:32 +0800

    Small changes to files

--- AIML_Models/Label_Sentiments.py
+++ AIML_Models/Label_Sentiments.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 from datetime import datetime
+import yfinance as yf
 
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
@@ -9,7 +10,6 @@ import torch
 model = AutoModelForSequenceClassification.from_pretrained("CryptoFinBertFinal")
 tokenizer = AutoTokenizer.from_pretrained("Tokenizer")
 
-filepath = Path('Labelled_Dataset.csv')
 
 def predict_label(text, model, tokenizer):
     inputs = tokenizer(text, truncation=True, padding="max_length", max_length=128, return_tensors="pt")
@@ -18,13 +18,12 @@ def predict_label(text, model, tokenizer):
     single_output = torch.argmax(predictions)
     return single_output.item()
 
-def label_news(csv_filepath):
-    df = pd.read_csv(csv_filepath)
+def label_news(csvFilepath):
+    df = pd.read_csv(csvFilepath)
     df["label"] = df["text"].apply(lambda x: predict_label(x, model, tokenizer))
-    df.to_csv(filepath)
     return df
 
-def output_to_LSTM(df):
+def labels_by_date(df):
     def weight_average(positive, negative, neutral):
         count = positive + negative + neutral
         if count == 0:
@@ -39,8 +38,18 @@ def output_to_LSTM(df):
 
 inputFileName = "LatestNews.csv"
 
-df = output_to_LSTM(label_news(inputFileName))
+df = labels_by_date(label_news(inputFileName))
+
+# Pull Price Data from YFinance
+startdate = df['Date'].min()
+enddate = df['Date'].max()
+data = yf.download("ETH-USD", start=startdate, end=enddate)
+df = data.merge(df, how="left", on="Date")
 print(df.head())
-today = datetime.now()
-outputFileName = ("LSTMOutput" + str(today)).replace(" ", "").replace(":", "-").replace(".", "") + ".csv"
-df.to_csv(outputFileName)
+
+LSTMDatasetFilepath = 'lstm_data.csv'
+df2 = pd.read_csv(LSTMDatasetFilepath)
+print(df2.head())
+# today = datetime.now()
+# outputFileName = ("LSTMOutput" + str(today)).replace(" ", "").replace(":", "-").replace(".", "") + ".csv"
+# df.to_csv(outputFileName)
